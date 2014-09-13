e909a25337ffbb90a371b88686034cfc492edf2e
Naftoreiclag <naftoreiclag@gmail.com>
2014-09-12 18:41:58 -0700

    Another failed attempt at a terrain mesh

--- src/naftoreiclag/villagefive/UnoptimizedHills.java
+++ src/naftoreiclag/villagefive/UnoptimizedHills.java
@@ -22,6 +22,7 @@ public class UnoptimizedHills
     public static final int numLevels = 16;
     
     public float[][] map = new float[width][height];
+    boolean[][][] sides = new boolean[width][height][2];
     
     public Mesh mesh;
     
@@ -45,6 +46,8 @@ public class UnoptimizedHills
             for(int z = 0; z < height; ++ z)
             {
                 map[x][z] = (img.getRGB(x, z) & 0x000000F0) / 16;
+                sides[x][z][0] = false;
+                sides[x][z][1] = false;
             }
         }
     }
@@ -66,10 +69,48 @@ public class UnoptimizedHills
         return map[x][z];
     }
     
+    public void foobar(int x, int z, int side)
+    {
+        if(side == 2)
+        {
+            sides[x + 1][z][0] = !sides[x + 1][z][0];
+        }
+        else if(side == 3)
+        {
+            sides[x][z + 1][1] = !sides[x][z + 1][1];
+        }
+        else
+        {
+            sides[x][z][side] = !sides[x][z][side];
+        }
+    }
+    
+    public boolean quux(int x, int z, int side)
+    {
+        if(side == 2)
+        {
+            return sides[x + 1][z][0];
+        }
+        else if(side == 3)
+        {
+            return sides[x][z + 1][1];
+        }
+        else
+        {
+            return sides[x][z][side];
+        }
+    }
+        
+    
     float vertu = 1.0f;
     float horzu = 0.125f;
     public void updateVertex(int x, int z)
     {
+    }
+    
+    private float smallest(float a, float b, float c, float d)
+    {
+        return a<b?(a<c?(a<d?a:d):(c<d?c:d)):(b<c?(b<d?b:d):(c<d?c:d));
     }
     
     public void buildGeometry()
@@ -85,67 +126,208 @@ public class UnoptimizedHills
         
         float limit = 2;
         
+        
         for(int x = 0; x < width - 1; ++ x)
         {
             for(int z = 0; z < height - 1; ++ z)
             {
-                // 1   2
-                //
-                // 4   3
+                // 
+                //       1
+                //     0   1
+                //   0   p   2
+                //     3   2
+                //       3
+                //       
                 
-                float h1 = access(x    , z    );
-                float h2 = access(x + 1, z    );
-                float h3 = access(x + 1, z + 1);
-                float h4 = access(x    , z + 1);
+                //   B
+                // A p C
+                //   D
                 
-                float smallest = smallest(h1, h2, h3, h4);
+                float h0 = access(x    , z    );
+                float h1 = access(x + 1, z    );
+                float h2 = access(x + 1, z + 1);
+                float h3 = access(x    , z + 1);
                 
-                mb.setAppendOrigin(x, smallest, z);
+                float sp = smallest(h0, h1, h2, h3);
                 
-                h1 -= smallest;
-                h2 -= smallest;
-                h3 -= smallest;
-                h4 -= smallest;
+                h0 -= sp;
+                h1 -= sp;
+                h2 -= sp;
+                h3 -= sp;
                 
+                if(h3 > limit || h0 > limit)
+                {
+                    foobar(x, z, 0);
+                }
+                if(h0 > limit || h1 > limit)
+                {
+                    foobar(x, z, 1);
+                }
+                if(h1 > limit || h2 > limit)
+                {
+                    foobar(x, z, 2);
+                }
+                if(h2 > limit || h3 > limit)
+                {
+                    foobar(x, z, 3);
+                }
+            }
+        }
+        
+        /*
+        for(int x = 0; x < width - 1; ++ x)
+        {
+            for(int z = 0; z < height - 1; ++ z)
+            {
+                // 
+                //       b
+                //     0   1
+                //   a   p   c
+                //     3   2
+                //       d
+                //       
+                
+                //   B
+                // A p C
+                //   D
+                
+                float h0 = access(x    , z    );
+                float h1 = access(x + 1, z    );
+                float h2 = access(x + 1, z + 1);
+                float h3 = access(x    , z + 1);
+                
+                float sp = smallest(h0, h1, h2, h3);
+                
+                mb.setAppendOrigin(x, sp, z);
+                
+                h0 -= sp;
+                h1 -= sp;
+                h2 -= sp;
+                h3 -= sp;
+                
+                float mh0 = h0 > limit ? limit : h0;
                 float mh1 = h1 > limit ? limit : h1;
                 float mh2 = h2 > limit ? limit : h2;
                 float mh3 = h3 > limit ? limit : h3;
-                float mh4 = h4 > limit ? limit : h4;
                 
                 mb.addQuad(
-                        0, mh1, 0, Vector3f.ZERO, 0, 0, 
-                        1, mh2, 0, Vector3f.ZERO, 1, 0, 
-                        1, mh3, 1, Vector3f.ZERO, 1, 1, 
-                        0, mh4, 1, Vector3f.ZERO, 0, 1);
+                        0, mh0, 0, Vector3f.ZERO, 0, 0, 
+                        1, mh1, 0, Vector3f.ZERO, 1, 0, 
+                        1, mh2, 1, Vector3f.ZERO, 1, 1, 
+                        0, mh3, 1, Vector3f.ZERO, 0, 1);
+                
+                if(quux(x, z, 0))
+                {
+                    mb.addQuad(
+                            0, h0, 0, Vector3f.ZERO, 0, 0, 
+                            1, h1, 0, Vector3f.ZERO, 1, 0, 
+                            1, mh1, 0, Vector3f.ZERO, 1, 1, 
+                            0, mh0, 0, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 1))
+                {
+                    mb.addQuad(
+                            1, h1, 0, Vector3f.ZERO, 0, 0, 
+                            1, h2, 1, Vector3f.ZERO, 1, 0, 
+                            1, mh2, 1, Vector3f.ZERO, 1, 1, 
+                            1, mh1, 0, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 2))
+                {
+                    mb.addQuad(
+                            1, h2, 1, Vector3f.ZERO, 0, 0, 
+                            0, h3, 1, Vector3f.ZERO, 1, 0, 
+                            0, mh3, 1, Vector3f.ZERO, 1, 1, 
+                            1, mh2, 1, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 3))
+                {
+                    mb.addQuad(
+                            0, h3, 1, Vector3f.ZERO, 0, 0, 
+                            0, h0, 0, Vector3f.ZERO, 1, 0, 
+                            0, mh0, 0, Vector3f.ZERO, 1, 1, 
+                            0, mh3, 1, Vector3f.ZERO, 0, 1);
+                }
+            }
+        }
+        */
+                
+        for(int x = 0; x < width - 1; ++ x)
+        {
+            for(int z = 0; z < height - 1; ++ z)
+            {
+                //     6   7
+                //       b
+                // 5   0   1   8
+                //   a   p   c
+                // 4   3   2   9
+                //       d
+                //     B   A
+                
+                //   B
+                // A p C
+                //   D
+                
+                float h0 = access(x    , z    );
+                float h1 = access(x + 1, z    );
+                float h2 = access(x + 1, z + 1);
+                float h3 = access(x    , z + 1);
+                
+                float sp = smallest(h0, h1, h2, h3);
+                
+                mb.setAppendOrigin(x, sp, z);
+                
+                h0 -= sp;
+                h1 -= sp;
+                h2 -= sp;
+                h3 -= sp;
+                
+                float mh0 = h0 > limit ? limit : h0;
+                float mh1 = h1 > limit ? limit : h1;
+                float mh2 = h2 > limit ? limit : h2;
+                float mh3 = h3 > limit ? limit : h3;
                 
                 mb.addQuad(
-                        0, h1, 0, Vector3f.ZERO, 0, 0, 
-                        1, h2, 0, Vector3f.ZERO, 1, 0, 
-                        1, mh2, 0, Vector3f.ZERO, 1, 1, 
-                        0, mh1, 0, Vector3f.ZERO, 0, 1);
-                mb.addQuad(
-                        1, h2, 0, Vector3f.ZERO, 0, 0, 
-                        1, h3, 1, Vector3f.ZERO, 1, 0, 
-                        1, mh3, 1, Vector3f.ZERO, 1, 1, 
-                        1, mh2, 0, Vector3f.ZERO, 0, 1);
-                mb.addQuad(
-                        1, h3, 1, Vector3f.ZERO, 0, 0, 
-                        0, h4, 1, Vector3f.ZERO, 1, 0, 
-                        0, mh4, 1, Vector3f.ZERO, 1, 1, 
-                        1, mh3, 1, Vector3f.ZERO, 0, 1);
-                mb.addQuad(
-                        0, h4, 1, Vector3f.ZERO, 0, 0, 
-                        0, h1, 0, Vector3f.ZERO, 1, 0, 
-                        0, mh1, 0, Vector3f.ZERO, 1, 1, 
-                        0, mh4, 1, Vector3f.ZERO, 0, 1);
+                        0, mh0, 0, Vector3f.ZERO, 0, 0, 
+                        1, mh1, 0, Vector3f.ZERO, 1, 0, 
+                        1, mh2, 1, Vector3f.ZERO, 1, 1, 
+                        0, mh3, 1, Vector3f.ZERO, 0, 1);
+                
+                if(quux(x, z, 1))
+                {
+                    mb.addQuad(
+                            0, h0, 0, Vector3f.ZERO, 0, 0, 
+                            1, h1, 0, Vector3f.ZERO, 1, 0, 
+                            1, mh1, 0, Vector3f.ZERO, 1, 1, 
+                            0, mh0, 0, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 2))
+                {
+                    mb.addQuad(
+                            1, h1, 0, Vector3f.ZERO, 0, 0, 
+                            1, h2, 1, Vector3f.ZERO, 1, 0, 
+                            1, mh2, 1, Vector3f.ZERO, 1, 1, 
+                            1, mh1, 0, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 3))
+                {
+                    mb.addQuad(
+                            1, h2, 1, Vector3f.ZERO, 0, 0, 
+                            0, h3, 1, Vector3f.ZERO, 1, 0, 
+                            0, mh3, 1, Vector3f.ZERO, 1, 1, 
+                            1, mh2, 1, Vector3f.ZERO, 0, 1);
+                }
+                if(quux(x, z, 0))
+                {
+                    mb.addQuad(
+                            0, h3, 1, Vector3f.ZERO, 0, 0, 
+                            0, h0, 0, Vector3f.ZERO, 1, 0, 
+                            0, mh0, 0, Vector3f.ZERO, 1, 1, 
+                            0, mh3, 1, Vector3f.ZERO, 0, 1);
+                }
             }
         }
         
         mesh = mb.bake(vertu, horzu, vertu);
-    }
-    
-    private float smallest(float a, float b, float c, float d)
-    {
-        return a<b?(a<c?(a<d?a:d):(c<d?c:d)):(b<c?(b<d?b:d):(c<d?c:d));
     }
 }
